52756c7b4ec5f28628e695e8a37f5e23ede38421
21IT147 <21IT147@charusat.edu.in>
2023-09-30 14:56:07 +0530

    Third commit

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ from view import GUI
 from controller import Controller
 from  model import evaluateExpression
 
-# Client code
+
 def main():
     """Main function."""
-    # Create an instance of QApplication
+
     pycalc = QApplication(sys.argv)
-    # Show the calculator's GUI
+
     view = GUI()
     view.show()
 
-	# Create instances of the model and the controller
+
     model = evaluateExpression
     Controller(model=model, view=view)
 
-    # Execute the calculator's main loop
+
     sys.exit(pycalc.exec_())
 
 if __name__ == '__main__':
